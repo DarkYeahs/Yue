a0c68a62c3d476cc4f4825678f6bced521154b88
darkyeahs <yeahschen@gmail.com>
2017-10-23 14:31:18 +0800

    init the project

---
+++ src/Yue.ts
@@ -0,0 +1,9 @@
+export default class Yue {
+
+  $options: any;
+
+
+  constructor (opt) {
+    this.$options = opt
+  }
+}
---
+++ test/unit/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+Vue.config.productionTip = false
+
+// require all test files (files that ends with .spec.ts)
+const testsContext = require.context('../../src', true, /\.spec$/)
+testsContext.keys().forEach(testsContext)
+
+// require all src files except main.ts for coverage.
+// you can also change this to match only the subset of files that
+// you want coverage for.
+const srcContext = require.context('../../src', true, /^\.\/(?!main\.ts$).+\.(ts|vue)$/i)
+srcContext.keys().forEach(srcContext)
