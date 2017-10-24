331eb007ff0101428112dc5b36fc7907e07b86c0
darkyeahs <yeahschen@gmail.com>
2017-10-24 17:16:42 +0800

    将Yue上的data数据映射到Yue上

--- src/Yue/Yue.ts
+++ src/Yue/Yue.ts
@@ -22,9 +22,43 @@ export class Yue {
     else {
       throw new Error('the data param should be function or object')
     }
-
+    this.initComputed()
+    this.proxyData()
     this.$observer = new Observer(this.$data)
     if (this.$el) this.compile(this.$el)
+  }
+
+  initComputed () {
+    let computed = this.$options.computed
+
+    if (typeof computed === 'object')
+      Object.keys(computed).forEach(key => {
+        Object.defineProperty(this, key, {
+          enumerable: true,
+          configurable: false,
+          set () {},
+          get: typeof computed[key] === 'function' ? computed[key] : computed.get
+        })
+      })
+  }
+
+  proxyData () {
+    let data = this.$data
+    let me = this
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        enumerable: true,
+        configurable: false,
+
+        set (newVal) {
+          me.$data[key] = newVal
+        },
+
+        get () {
+          return me.$data[key]
+        }
+      })
+    })
   }
 
   compile (el: string) {
@@ -35,4 +69,5 @@ export class Yue {
     this.$el = el
     this.compile(this.$el)
   }
+
 }
--- src/Yue/observer.ts
+++ src/Yue/observer.ts
@@ -39,7 +39,6 @@ export default class Observer {
         return val
       }
     })
-
   }
 
   obseverChild (val) {
